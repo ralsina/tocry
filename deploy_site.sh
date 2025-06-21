cdf7550dd806fd4490cf3bf8cb3ee8b8a462ca9e
Roberto Alsina <roberto.alsina@gmail.com>
2025-06-21 19:58:28 -0300

    fix: use left/right to scroll lanes

--- deploy_site.sh
+++ deploy_site.sh
@@ -2,4 +2,4 @@
 set -e
 
 rsync -rav website.html rocky:/data/stacks/web/websites/tocry.ralsina.me/index.html
-rsync -rav src/assets/favicon.ico screenshot.png rocky:/data/stacks/web/websites/tocry.ralsina.me/favicon.ico
+rsync -rav src/assets/favicon.ico screenshot.png rocky:/data/stacks/web/websites/tocry.ralsina.me/
