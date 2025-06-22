0c46036cb9d10afec9edb71c270cc8bc46727162
Roberto Alsina <roberto.alsina@gmail.com>
2025-06-22 18:23:49 -0300

    chore: dumb deployment script

--- deploy_site.sh
+++ deploy_site.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-rsync -rav website.html rocky:/data/stacks/web/websites/tocry.ralsina.me/index.html
-rsync -rav src/assets/favicon.ico screenshot.png rocky:/data/stacks/web/websites/tocry.ralsina.me/
+rsync -rav src/assets/website.html rocky:/data/stacks/web/websites/tocry.ralsina.me/index.html
+rsync -rav src/assets/favicon.ico screenshot.png src/assets/*.css rocky:/data/stacks/web/websites/tocry.ralsina.me/
