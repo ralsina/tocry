c46d896702f02a4dba8151f05efe9f87a986fe71
Roberto Alsina <roberto.alsina@gmail.com>
2025-06-23 19:30:54 -0300

    build: make binaries smaller

--- build_static.sh
+++ build_static.sh
@@ -10,11 +10,11 @@ shards install
 # Build for AMD64
 docker build . -f Dockerfile.static -t tocry-builder
 docker run -ti --rm -v "$PWD":/app --user="$UID" tocry-builder \
-  /bin/sh -c "cd /app && shards build --static --release && strip bin/tocry"
+  /bin/sh -c "cd /app && shards build --static --release && strip bin/tocry && upx bin/tocry"
 mv bin/tocry bin/tocry-static-linux-amd64
 
 # Build for ARM64
 docker build . -f Dockerfile.static --platform linux/arm64 -t tocry-builder
 docker run -ti --rm -v "$PWD":/app --platform linux/arm64 --user="$UID" tocry-builder \
-  /bin/sh -c "cd /app && shards build --static --release && strip bin/tocry"
+  /bin/sh -c "cd /app && shards build --static --release && strip bin/tocry && upx bin/tocry"
 mv bin/tocry bin/tocry-static-linux-arm64
