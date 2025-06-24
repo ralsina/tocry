f54b6142dec8898b2f51d25a80f801fceb31e2f6
Roberto Alsina <roberto.alsina@gmail.com>
2025-06-23 21:24:35 -0300

    chore: build tweak

--- do_release.sh
+++ do_release.sh
@@ -10,6 +10,7 @@ git add shard.yml
 # hace lint test
 git cliff --bump -o
 git commit -a -m "bump: Release v$VERSION"
+git push
 git tag "v$VERSION"
 git push --tags
 gh release create "v$VERSION" "bin/$PKGNAME-static-linux-amd64" "bin/$PKGNAME-static-linux-arm64" --title "Release v$VERSION" --notes "$(git cliff -l -s all)"
