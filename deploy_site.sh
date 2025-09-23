1aa396f563c2a65b1aef413eda0715cca6e59970
Roberto Alsina <roberto.alsina@gmail.com>
2025-09-23 11:14:41 -0300

    docs: general improvements and fixes

--- deploy_site.sh
+++ deploy_site.sh
@@ -2,4 +2,4 @@
 set -e
 
 rsync -rav src/assets/website.html rocky:/data/stacks/web/websites/tocry.ralsina.me/index.html
-rsync -rav src/assets/favicon.ico screenshot.png src/assets/*.css rocky:/data/stacks/web/websites/tocry.ralsina.me/
+rsync -rav src/assets/favicon.ico screenshot.png src/assets/*.css install.sh rocky:/data/stacks/web/websites/tocry.ralsina.me/
